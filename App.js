f06179cf6c657197a4eb3c961ddba1af44f7fe3a
psychehose <psychehose@gmail.com>
2022-04-18 17:33:31 +0900

    init

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, Text } from "react-native";
+import AppLoading from "expo-app-loading";
+import { useState } from "react";
+
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  // preLoad만 가능
+  const [assets] = useAssets([]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+
+  if (!assets || !loaded) {
+    return <AppLoading />;
+  }
+
+  return null;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// 이 방식은 preFetch도 가능
+
+// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+// const loadImages = (images) =>
+//   images.map((image) => {
+//     if (typeof image === "string") {
+//       return Image.prefetch(image);
+//     } else {
+//       return Asset.loadAsync(image);
+//     }
+//   });
+
+// export default function App() {
+
+// const [ready, setReady] = useState(false);
+// const onFinish = () => setReady(true);
+// const startLoading = async () => {
+//   const fonts = loadFonts([Ionicons.font]);
+
+//   await Promise.all(...fonts);
+// };
+
+// if (!ready) {
+//   return (
+//     <AppLoading
+//       startAsync={startLoading}
+//       onFinish={onFinish}
+//       onError={console.error}
+//     />
+//   );
+// }
+// return null;
+// }
