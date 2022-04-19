cbf3e16f541737d01501ad19becf25e21304bf00
psychehose <psychehose@gmail.com>
2022-04-19 15:27:32 +0900

    navigation

--- App.js
+++ App.js
@@ -6,50 +6,41 @@ import { useState } from "react";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
+
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
-  // preLoad만 가능
-  const [assets] = useAssets([]);
-  const [loaded] = Font.useFonts(Ionicons.font);
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    const fonts = loadFonts([Ionicons.font]);
 
-  if (!assets || !loaded) {
-    return <AppLoading />;
+    await Promise.all(...fonts);
+  };
+
+  if (!ready) {
+    return (
+      <AppLoading
+        startAsync={startLoading}
+        onFinish={onFinish}
+        onError={console.error}
+      />
+    );
   }
-
-  return null;
+  return (
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
+  );
 }
-
-// 이 방식은 preFetch도 가능
-
-// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-// const loadImages = (images) =>
-//   images.map((image) => {
-//     if (typeof image === "string") {
-//       return Image.prefetch(image);
-//     } else {
-//       return Asset.loadAsync(image);
-//     }
-//   });
-
-// export default function App() {
-
-// const [ready, setReady] = useState(false);
-// const onFinish = () => setReady(true);
-// const startLoading = async () => {
-//   const fonts = loadFonts([Ionicons.font]);
-
-//   await Promise.all(...fonts);
-// };
-
-// if (!ready) {
-//   return (
-//     <AppLoading
-//       startAsync={startLoading}
-//       onFinish={onFinish}
-//       onError={console.error}
-//     />
-//   );
-// }
-// return null;
-// }
